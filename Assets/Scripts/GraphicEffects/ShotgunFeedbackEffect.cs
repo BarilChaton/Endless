a5d48a0b9e1c495b187b0e0d9803b1d076c2df2d
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-28 13:49:24 +0200

    merging to work with shotgun feedback
    as it says

---
+++ Assets/Scripts/GraphicEffects/ShotgunFeedbackEffect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunFeedbackEffect : MonoBehaviour
+{
+    public float lifeTime;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
