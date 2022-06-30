4eb030fe9813945c73907a7db2a5d16aebc801c4
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-06-30 13:06:54 +0200

    Shotgun spread wip

--- Assets/Scripts/Player/Guns/GunCore.cs
+++ Assets/Scripts/Player/Guns/GunCore.cs
@@ -16,6 +16,9 @@ namespace Endless.PlayerCore
         [HideInInspector] public int AmmoPerClip;
         [SerializeField] public int CurrentTotalAmmo;
         [HideInInspector] public int CurrentAmmo;
+        [Header("Gun Details")]
+        [SerializeField] public bool isShotgun;
+        [SerializeField] public int bulletsPerShot = 1;
         [Header("Art stuff")]
         [SerializeField] public GameObject TempBulletImpact;
         public Animator gunAnim;
@@ -27,33 +30,36 @@ namespace Endless.PlayerCore
             {
                 if (CurrentTotalAmmo > 0)
                 {
-                    Ray ray = playerCamera.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+                    Ray[] rays;
+                    if (isShotgun) rays = ShotgunStuff(playerCamera);
+                    else rays = NotShotgunStuff(playerCamera);
 
                     // Checking if it hit anything
-                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    foreach (Ray r in rays)
                     {
-                        //Debug.Log("I'm shooting at" + hit.transform.name);
-
-                        // Hitting an enemy specifically
-                        if (hit.transform.tag == "Enemy")
+                        if (Physics.Raycast(r, out RaycastHit hit))
                         {
-                            float damage = DamageAmt;
-                            hit.collider.GetComponent<EnemyCore>().TakeDamage(damage);
+                            // Hitting an enemy specifically
+                            if (hit.transform.tag == "Enemy")
+                            {
+                                float damage = DamageAmt;
+                                hit.collider.GetComponent<EnemyCore>().TakeDamage(damage);
+                            }
+
+                            // Hitting anything else. TODO: Hitting decoration? Lamps? -- TBI
+                            else
+                            {
+                                Instantiate(TempBulletImpact, hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal));
+                            }
                         }
 
-                        // Hitting anything else. TODO: Hitting decoration? Lamps? -- TBI
+
+                        // Hit fuckall
                         else
                         {
-                            Instantiate(TempBulletImpact, hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal));
+                            Debug.Log("I'm looking at nothing");
                         }
                     }
-
-                    // Hit fuckall
-                    else
-                    {
-                        Debug.Log("I'm looking at nothing");
-                    }
-
                     // Removing ammo and doing animations
                     CurrentTotalAmmo--;
                     gunAnim.Play("Shoot");
@@ -66,6 +72,31 @@ namespace Endless.PlayerCore
 
             // ratatatatatattatatata
 
+        }
+
+        private Ray[] ShotgunStuff(Camera playerCamera = null)
+        {
+            Vector2[] points = new Vector2[5];
+            GaussianDistribution gd = new GaussianDistribution(); // maybe send a Random state through the ctor? I don't really use Unity's random any more
+            for (int i = 0; i < bulletsPerShot; i++)
+            {
+                points[i] = new Vector2(gd.Next(0f, 1f, -1f, 1f), gd.Next(0f, 1f, -1f, 1f));
+            }
+
+            Ray[] rays = new Ray[points.Length];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Vector3 p3d = new Vector3(points[i].x, points[i].y, 6f);
+                rays[i] = new Ray(Vector3.zero, p3d.normalized);                
+            }
+            return rays;
+        }
+
+        private Ray[] NotShotgunStuff(Camera playerCamera = null)
+        {
+            Ray[] ray = new Ray[1];
+            ray[0] = playerCamera.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+            return ray;
         }
     }
 }
