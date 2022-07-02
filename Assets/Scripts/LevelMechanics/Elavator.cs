0d6c76f7638b7372d9f572bd74f97586410c7c4e
Shinela <real.shinela@gmail.com>
2022-07-02 22:57:25 +0200

    update lift

--- Assets/Scripts/LevelMechanics/Elavator.cs
+++ Assets/Scripts/LevelMechanics/Elavator.cs
@@ -2,57 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elavator : MonoBehaviour
+public class Lift : MonoBehaviour
 {
-    [SerializeField] private Vector3 origin, target;
-    [SerializeField] private float speed;
-    [SerializeField] private float setTime = 2;
+    // 2.81 seems to be the sweet spot for this one. Will have to recalculate if you want another floor.
+    [SerializeField] private Vector3 floorDistance = new Vector3(0, 2.18f, 0);
 
-    private float timer;
-    private bool timeToGo;
-    private bool goingUp = false;
-    private bool playerOnTop = false;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float returnTimer = 10f;
+    [SerializeField] private float activationTimer = 0f;
+    [SerializeField] private int floor = 0;
+    [SerializeField] private int maxFloor = 1;
 
-    private void Awake()
+    // In case you want something ELSE to move
+    [SerializeField] private Transform moveTransform;
+
+    private float tTotal;
+    private bool isMoving;
+    private float moveDirection;
+    private float invokeTimer;
+
+    void Start()
     {
-        timer = setTime;
+        moveTransform = moveTransform != null ? moveTransform : transform;
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        if (!goingUp && playerOnTop)
+        // If ismoving is activated, we move
+        if (isMoving)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-            if (transform.position == target)
-            {
-                goingUp = true;
-                Invoke("TimeToGoBack", setTime);
-            }
+            MoveElevator();
         }
-        else if (transform.position == origin)
+    }
+
+    void MoveElevator()
+    {
+        // Setting the move directions
+        // I found it easier to just move it x distance instead of giving it destinations
+        Vector3 v = moveDirection * floorDistance * speed;
+        float t = Time.deltaTime;
+        float tMax = floorDistance.magnitude / speed;
+        t = Mathf.Min(t, tMax - tTotal);
+
+        // Actually moving
+        moveTransform.Translate(v * t);
+        tTotal += t;
+
+        // landing
+        if (tTotal >= tMax)
         {
-            goingUp = false;
+            isMoving = false;
+            tTotal = 0;
         }
-        //else if (goingUp && playerOnTop && timeToGo)
-        //{
-        //    transform.position = Vector3.MoveTowards(transform.position, origin, +speed * Time.deltaTime);
-        //    if (transform.position == origin)
-        //    {
-        //        goingUp = false;
-        //    }
-        //}
-        //else if (timeToGo)
-        //{
-        //    transform.position = Vector3.MoveTowards(transform.position, origin, +speed * Time.deltaTime);
-        //}
+    }
+
+    public void StartMoveUp()
+    {
+        // so you can't restart move as it moves
+        if (isMoving) return;
+
+        // Starting return timer
+        Invoke("StartMoveDown", returnTimer);
+
+        // movement starts, up or down (up here), and add/remove a floor
+        isMoving = true;
+        moveDirection = 1;
+        floor += 1;
+    }
+
+    public void StartMoveDown()
+    {
+        // same as above idk what you want lmao
+        if (isMoving) return;
+
+        isMoving = true;
+        moveDirection = -1;
+        floor -= 1;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        // You know this part :stuck_out_tongue:
+        if (other.tag == "Player" && invokeTimer < Time.time)
         {
-            playerOnTop = true;
             other.transform.parent = this.transform;
+            if (floor != maxFloor)
+            {
+                Invoke(nameof(StartMoveUp), activationTimer);
+                invokeTimer = Time.time + returnTimer;
+            }
+            else if (floor != 0) Invoke(nameof(StartMoveDown), activationTimer);
         }
     }
 
@@ -64,10 +103,4 @@ public class Elavator : MonoBehaviour
         }
     }
 
-    private void TimeToGoBack()
-    {
-        playerOnTop = false;
-        goingUp = false;
-        transform.position = Vector3.MoveTowards(transform.position, origin, speed * Time.deltaTime);
-    }
 }
