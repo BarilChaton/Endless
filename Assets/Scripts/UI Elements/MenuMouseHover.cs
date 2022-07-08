bfb340c5f5a76360eea6f35caa82d06948a4fa25
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-07-08 09:54:51 +0200

    Update MenuMouseHover.cs

--- Assets/Scripts/UI Elements/MenuMouseHover.cs
+++ Assets/Scripts/UI Elements/MenuMouseHover.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 
 public class MenuMouseHover : MonoBehaviour
 {
+	private float FontTemp;
+
 	void Start()
 	{
+		FontTemp = GetComponent<TextMeshPro>().fontSize;
 		GetComponent<TextMeshPro>().color = Color.black;
 	}
 
 	void OnMouseEnter()
 	{
 		GetComponent<TextMeshPro>().color = Color.red;
-		GetComponent<TextMeshPro>().fontSize = Color.red;
+		GetComponent<TextMeshPro>().fontSize = FontTemp * 1.2f;
 
 	}
 
 	void OnMouseExit()
 	{
 		GetComponent<TextMeshPro>().color = Color.black;
+		GetComponent<TextMeshPro>().fontSize = FontTemp;
 	}
 }
